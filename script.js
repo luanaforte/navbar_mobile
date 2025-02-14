b290679f0a83de0425cdcf1614ad26ee5ac30760
luanaforte <luanaforte950@gmail.com>
2025-02-14 16:38:14 -0300

    barra de navegação responsiva

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const navList = document.querySelector(".nav-list");
+
+hamburgerBtn.addEventListener("click", () => {
+    navList.classList.toggle("active")
+})
